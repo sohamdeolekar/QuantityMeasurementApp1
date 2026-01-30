39cf2b79aada9d8adf96b55fbb1a745333a69a56
sohamdeolekar <sohamdeolekar@gmail.com>
2026-01-30 21:51:10 +0530

    Test Cases Added

--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -1,4 +1,51 @@
 package com.apps.quantitymeasurement;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class QuantityMeasurementAppTest {
+
+    @Test
+    void testFeetEquality_SameValue() {
+        QuantityMeasurementApp.Feet feet1 =
+                new QuantityMeasurementApp.Feet(5.0);
+        QuantityMeasurementApp.Feet feet2 =
+                new QuantityMeasurementApp.Feet(5.0);
+
+        assertTrue(feet1.equals(feet2));
+    }
+
+    @Test
+    void testFeetEquality_DifferentValues() {
+        QuantityMeasurementApp.Feet feet1 =
+                new QuantityMeasurementApp.Feet(5.0);
+        QuantityMeasurementApp.Feet feet2 =
+                new QuantityMeasurementApp.Feet(6.0);
+
+        assertFalse(feet1.equals(feet2));
+    }
+
+    @Test
+    void testFeetEquality_SameReference() {
+        QuantityMeasurementApp.Feet feet =
+                new QuantityMeasurementApp.Feet(5.0);
+
+        assertTrue(feet.equals(feet));
+    }
+
+    @Test
+    void testFeetEquality_NullComparison() {
+        QuantityMeasurementApp.Feet feet =
+                new QuantityMeasurementApp.Feet(5.0);
+
+        assertFalse(feet.equals(null));
+    }
+
+    @Test
+    void testFeetEquality_DifferentObject() {
+        QuantityMeasurementApp.Feet feet =
+                new QuantityMeasurementApp.Feet(5.0);
+
+        assertFalse(feet.equals("5.0"));
+    }
 }
